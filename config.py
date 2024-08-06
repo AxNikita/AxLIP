d29d2e62c25540c029f7c0039029f07ea1d4c3dd
Nikita <kotovnikita00744@gmail.com>
2024-08-06 11:49:44 +0700

    feature-2: Add logging for all modules

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ from os import getenv
 
 TG_TOKEN = getenv("TG_TOKEN")
 
-GATEWAY_URL = "http://gateway-service:9999"
+# GATEWAY_URL = "http://gateway-service:9999"
+GATEWAY_URL = "http://localhost:9999"
--- sender_service.py
+++ sender_service.py
@@ -1,14 +1,20 @@
+import logging
+
 import requests
+
 import config
 
+logging = logging.getLogger("app")
+
 url = config.GATEWAY_URL + "/page"
+headers = {'Content-Type': 'application/json'}
+
 
 def send_page(page):
     payload = {'service': 'telegram', 'page': page}
-    response = requests.post(url, json=payload)
+    response = requests.post(url, json=payload, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        print(data)
+        logging.debug(str(response))
     else:
-        print(f"Ошибка: {response.status_code}")
+        logging.error(response.status_code)
